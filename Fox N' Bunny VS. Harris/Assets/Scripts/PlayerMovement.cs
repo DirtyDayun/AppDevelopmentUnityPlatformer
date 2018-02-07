26ad1497a3895c2d6f2deadf762b38273671960d
DirtyDayun <explodi>
2018-02-07 10:12:29 -0600

    Movement for the player

--- Fox N' Bunny VS. Harris/Assets/Scripts/PlayerMovement.cs
+++ Fox N' Bunny VS. Harris/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float speed;
 	public string keyUp;
 	public string keyDown;
 	public string keyLeft;
 	public string keyRight;
 
-	private float speed;
+	private Rigidbody2D rb;
+	private Vector2 velocity;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Movement ();
+	}
+
+	void Movement() {
+		rb.velocity = velocity;
+
+		if (Input.GetKey (keyLeft)) {
+			velocity.x = -speed;
+		} else if (Input.GetKey (keyRight)) {
+			velocity.x = speed;
+		} else {
+			velocity.x = 0;
+		}
 	}
 }
