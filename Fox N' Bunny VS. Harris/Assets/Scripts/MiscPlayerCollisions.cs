fdaf7b91d9abebd19c6e2acd4d440025a2c9a8fa
DirtyDayun <32467510+DirtyDayun@users.noreply.github.com>
2018-02-19 17:44:42 -0600

    Animation to fox

---
+++ Fox N' Bunny VS. Harris/Assets/Scripts/MiscPlayerCollisions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiscPlayerCollisions : MonoBehaviour {
+
+    PlayerMovement player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Cloud")
+        {
+            if (player.velocity.y <= 0)
+            {
+                player.slowFall = true;
+                player.isGrounded = true;
+            }
+        }
+    }
+
+    void OnTriggerLeave2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Cloud")
+        {
+            player.slowFall = false;
+        }
+    }
+}
--- Fox N' Bunny VS. Harris/Assets/Scripts/PlayerMovement.cs
+++ Fox N' Bunny VS. Harris/Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float jumpSpeed;
+    public Vector2 velocity; // This Vector2 is used to change the Rigidbody2D's velocity easily
+    public float jumpSpeed;
 	public float gravity;
 	public float speed;
 	public string keyUp;
 	public string keyDown;
 	public string keyLeft;
 	public string keyRight;
+    public bool slowFall;
+    public bool isGrounded;
 
-	private Rigidbody2D rb;
-	private Vector2 velocity; // This Vector2 is used to change the Rigidbody2D's velocity easily
-	private bool isGrounded;
+    private Rigidbody2D rb;
+    private Animator animation;
+    private bool animPlaying;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+        animation = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -34,22 +38,47 @@ public class PlayerMovement : MonoBehaviour {
 
 		if (Input.GetKey (keyLeft)) {
 			velocity.x = -speed; // -speed (or speed * -1) is set to the x velocity to make the player go left
+            transform.localScale = new Vector3(-4, 4, 1);
+
+            if (isGrounded && !animPlaying && velocity.y <= 0)
+            {
+                animation.Play("Walk");
+            }
 		} else if (Input.GetKey (keyRight)) {
 			velocity.x = speed; // speed is set to the x velocity to make the player go right
-		} else {
+            transform.localScale = new Vector3(4, 4, 1);
+
+            if (isGrounded && !animPlaying && velocity.y <= 0)
+            {
+                animation.Play("Walk");
+            }
+        } else {
 			velocity.x = 0; // If the left/right keys aren't pressed, the player stops moving horizontally
-		}
+
+            if (isGrounded && velocity.y <= 0)
+            {
+                animation.Play("Stand");
+                animPlaying = false;
+            }
+        }
 
 		if (Input.GetKey (keyUp) && isGrounded) {
 			Jump (); // The Jump method is called which controls jumping, wow!
-		}
+            animation.Play("Jump");
+            animPlaying = false;
+        }
 	}
 
     // The Gravity method deals with... well... gravity
 	void Gravity() {
-		if (velocity.y > -15) {
+		if (velocity.y > -15 && !slowFall) {
 			velocity.y -= gravity;
 		}
+
+        if (slowFall)
+        {
+            velocity.y = -0.35f;
+        }
 
         // These raycasts are cast downwards on both sides of the player to detect if the player is standing on ground
 		RaycastHit2D ray1 = Physics2D.Raycast(transform.position - new Vector3(0.5f, 0.25f), Vector2.down, 0.5f);
@@ -68,5 +97,11 @@ public class PlayerMovement : MonoBehaviour {
 		velocity.y = jumpSpeed; // The y velocity is set to jumpSpeed to make the player... jump!
                                 // gravity constantly removes from the y velocity so the player
                                 // eventually falls back down
+        slowFall = false;
 	}
+
+    public void Animation()
+    {
+        animPlaying = true;
+    }
 }
